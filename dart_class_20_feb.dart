9814d4b8738ed0363e5973b4e2fa2bd48e820f46
Hafsa Mateen <hafsa90989@gmail.com>
2026-03-12 11:18:57 +0500

    first commit

---
+++ dart_class_20_feb.dart
@@ -0,0 +1,18 @@
+void main(){
+
+ int start = 0;
+
+//  while(start<0){
+//   print("Hellow $start");
+//   start++;
+//  }
+
+do {
+  print("Hello $start");
+  start++;
+}
+while(start<10);
+
+var a = 9;
+print(a);
+}
